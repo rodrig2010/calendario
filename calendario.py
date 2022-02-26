06b5b58762e733abfd99af159667d38e940d82ee
rodrigo <rodrigokaconde2010@hotmail.com>
2022-02-26 17:20:46 -0300

    commit

---
+++ calendario.py
@@ -0,0 +1,35 @@
+import calendar
+from tkinter import *
+
+
+
+
+def ShowCalendar():
+    new_gui = Tk()
+    new_gui.config(background="White")
+    new_gui.title("CALENDAR")
+    new_gui.geometry("550x600")
+    fetch_year = int(year_field.get())
+    cal_content = calendar.calendar(fetch_year)
+    cal_year = Label(new_gui, text = cal_content, font="Consolas 10 bold")
+    cal_year.grid(row = 5, column=1, padx=20)
+    new_gui.mainloop()
+
+if __name__ == "__main__":
+    gui = Tk()
+    gui.config(background="White")
+    gui.title("Calendar")
+    gui.geometry("250x140")
+    cal = Label(gui, text="CALENDAR", bg="dark gray", font=("times", 28, 'bold'))
+    year = Label(gui, text="ENTER YEAR", bg="light green")
+    year_field = Entry(gui)
+    Show = Button(gui, text="Show Calendar", fg="Black", bg="Red", command=ShowCalendar)
+    Exit = Button(gui, text="EXIT", fg="Black", bg="Red", command = exit )
+
+    cal.grid(row=1, column=1)
+    year.grid(row=2, column=1)
+    year_field.grid(row=3, column=1)
+    Show.grid(row=4, column=1)
+    Exit.grid(row=6, column=1)
+
+gui.mainloop()
